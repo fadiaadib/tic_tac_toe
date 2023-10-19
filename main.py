325f0f9a4f5765fdb15125afa6288ecb46866c42
Fadi Adib <fadi.adib@aura-sys.com>
2023-10-19 01:26:55 -0700

    Update Readme file

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from game import Game
 from banner import banner
 
 print(banner)
-game = Game(auto=input('Select number of players (1) or (2): ') != '2')
+game = Game(auto=input('Choose number of players (1) or (2): ') != '2')
 while True:
     game.make_move()
     game.display_board()
